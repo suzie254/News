32fba1eb6b4171481730c97c95180ee0a184e244
suzie254 <suzieakinyi08@gmail.com>
2020-07-14 13:43:02 +0300

    Imported the config class from config_options

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,25 @@
 from flask import Flask
-from .config import DevConfig 
 from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+	app = Flask(__name__)
+
+	#create the app configurations
+	app.config.from_object(config_options[config_name])
+
+	# Initializing flask extensions
+	bootstrap.init_app(app)
 
 
-#Initializing news_highlight
-app =   Flask(__name__,instance_relative_config = True)
+	#registering the blueprint
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
 
-#Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+	#setting config
+	from .request import configure_request
+	configure_request(app)
 
-#Initializing Flask extensions
-bootstrap = Bootstrap(app)
-
-
-from app import views
-from app import error
+	return app
